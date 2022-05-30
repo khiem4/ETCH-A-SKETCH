3a799ffc303ad6ad6c9ad571fdba5bba0caf0f27
khiem4 <khiemcan@gmail.com>
2022-05-30 23:17:30 +0700

    Create function for both type of div and add hover EventListener

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 const container = document.querySelector('#container');
-
 let horizontal;
 let vertical;
 
-for (let i = 0; i < 16; i++) {
+function height() {
     vertical = document.createElement('div');
-    vertical.classList.add('cell');
-    vertical.textContent = 'Text'
+    vertical.classList.add('div');
+    vertical.addEventListener("mouseenter", function (e) {
+        e.target.style.backgroundColor = 'yellow';
+    });
+    vertical.addEventListener("mouseleave", function (e) {
+        e.target.style.backgroundColor = "white";
+    });
     container.appendChild(vertical);
-    for (let j = 0; j < 16; j++) {
-        horizontal = document.createElement('div');
-        horizontal.classList.add('div');
-        container.appendChild(horizontal);
-    }
 }
 
+function width() {
+    horizontal = document.createElement('div');
+    horizontal.classList.add('div');
+    horizontal.addEventListener('mouseenter', function (e) {
+        e.target.style.backgroundColor = 'yellow';
+    });
+    horizontal.addEventListener("mouseleave", function (e) {
+        e.target.style.backgroundColor = "white";
+    });
+    container.appendChild(horizontal);
+}
+
+for (let i = 0; i < 16; i++) {
+    height();
+    for (let j = 0; j < 16; j++) {
+        width();
+    }
+}
 
 
 
